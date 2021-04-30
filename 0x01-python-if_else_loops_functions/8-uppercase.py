84b06d17e49ec23fe27d377253b790c9fee3ca54
Alexander Rodriguez <alexander.0894-rp@hotmail.com>
2021-04-30 02:28:05 +0000

    uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 def uppercase(str):
-
     for c in str:
         scan = ord(c)
         if scan >= 97 and scan <= 122:
             c = chr(scan - 32)
-            print("{}".format(c), end='')
+        print("{}".format(c), end='')
     print("")
