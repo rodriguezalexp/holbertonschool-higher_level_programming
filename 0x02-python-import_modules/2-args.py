904b54e8a1a8d19d2d18c7e3751d4bc09e9f23f7
Alexander Rodriguez <alexander.0894-rp@hotmail.com>
2021-05-06 00:59:42 +0000

    len arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,12 +3,8 @@ from sys import argv
 
 l = len(argv)
 
-if l <= 2 and l != 0:
-    print("{:d} {:s}".format(l - 1, "argument."))
-
-else:
-    print("{:d} {:s}".format(l - 1, "arguments:"))
-
+print("{:d} {:s} {:s}".format(l - 1, "arguments" if l <= 2 else "arguments",
+                              "."if l == 1 else ":"))
 for i, s in enumerate(argv):
     if i > 0:
         print("{:d}: {:s}".format(i, s))
