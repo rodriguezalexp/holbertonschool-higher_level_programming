3fcfaf7dbaf7c1020c1aaa901e66f1f11a6c10c4
Alexander Rodriguez <alexander.0894-rp@hotmail.com>
2021-05-21 00:47:05 +0000

    define setter and getter of height and width methods

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Rectangle class definition"""
+"""
+Rectangle class definition
+"""
 
 
 class Rectangle:
@@ -21,7 +23,7 @@ class Rectangle:
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -35,5 +37,5 @@ class Rectangle:
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
