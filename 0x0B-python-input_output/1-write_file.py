80a122cd3755257f4f441786281e9b87cbcd5a36
Alexander Rodriguez <alexander.0894-rp@hotmail.com>
2021-06-02 22:06:51 +0000

    write and return the len of a txt

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """return the len of txt file"""
-    with open(filename, 'W', encoding="utf-8") as f:
+    with open(filename, 'w', encoding="utf-8") as f:
         return f.write(text)
