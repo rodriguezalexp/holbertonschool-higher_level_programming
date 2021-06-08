a20ddbc8381acd7b5713d8bd851d71f789e2aff4
Alexander Rodriguez <alexander.0894-rp@hotmail.com>
2021-06-08 21:03:40 +0000

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -39,7 +39,7 @@ class Rectangle(Base):
         """setter for the private instance atributte width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        elif value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
 
@@ -48,7 +48,7 @@ class Rectangle(Base):
         """setter for the private instance atributte height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        elif value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
 
