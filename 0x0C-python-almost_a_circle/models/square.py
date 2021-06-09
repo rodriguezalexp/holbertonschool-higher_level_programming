e3b9009c58f556772897f8fa507d848d38d5bbd6
Alexander Rodriguez <alexander.0894-rp@hotmail.com>
2021-06-09 03:50:28 +0000

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,23 +10,21 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """square constructor"""
-    super().__init__(size, size, x, y, id)
-    self.size = size
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    def __str__(self):
+        """string representation of the square"""
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(
+            self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
         """getter for the size"""
         return self.width
 
-    @setter
+    @size.setter
     def size(self, value):
         """setter for size"""
         self.width = value
         self.height = value
-
-    def __str__(self):
-        """string representation of the square"""
-        return "[Square] ({:d}) {:d}/{:d} - {:d}".format{self.id,
-                                                         self.x,
-                                                         self.y,
-                                                         self.width}
