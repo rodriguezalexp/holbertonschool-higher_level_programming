a0861eab787fdbcf777c1eaa36c60a5cc6d1d363
Alexander Rodriguez <alexander.0894-rp@hotmail.com>
2021-06-14 22:08:14 +0000

    print comb

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,3 +1,5 @@
-#!/usr/bin/python3
-def islower(c):
-    return(ord('a') <= ord(c) <= ord('z'))
+#!/usr/bin/python
+for i in range(0, 8):
+    for j in range(i + 1, 10):
+        print("{:d} {:d}".format(i, j), end=", ")
+    print("{:d} {:d}".format(i + 1, j))
