5d04b5a3a5227708742c02ad94ba916982282532
Alexander Rodriguez <alexander.0894-rp@hotmail.com>
2021-06-16 22:10:51 +0000

    return len and firts char of a string

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""function with two returns"""
+
+
 def multiple_returns(sentence):
-    return len(sentence), sentence[0]
+    """return lenght and fir char of a string"""
+    if not sentence:
+        return len(sentence), 0
+    else:
+        return len(sentence), sentence[0]
