03cf0a14361e9303e04c23e09efc006058a3cdbb
Alexander Rodriguez <alexander.0894-rp@hotmail.com>
2021-06-18 00:27:56 +0000

    funtion that find all multiplies of 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""function to return bolean """
+
+
+def divisible_by_2(my_list=[]):
+	"""return true or false"""
+    new_list = []
+    for i in my_list:
+        if i % 2 == 0:
+            new.append(True)
+        elif i % 2 != 0:
+            new.append(False)
+        return new_list
