6a51750b32b5457fbd730d3b317d7c9cbf371079
Alexander Rodriguez <alexander.0894-rp@hotmail.com>
2021-06-20 21:46:29 +0000

    returns a set of all elements present in only one set.

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""function to return number of keys in a dictionary"""
+
+
+def number_keys(a_dictionary):
+	"""return number of key in dictionary"""
+    return len(a_dictionary)
