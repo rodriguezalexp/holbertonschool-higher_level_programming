28f34bc0f7879a0a1b0f065b42c78467df40f53f
Alexander Rodriguez <alexander.0894-rp@hotmail.com>
2021-09-28 00:55:54 +0000

    print n elements of a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    aux = 0
     for i in range(x):
         try:
             print(my_list[i], end="")
-            aux += 1
+            i += 1
         except IndexError:
             break
     print()
-    return aux
+    return i
