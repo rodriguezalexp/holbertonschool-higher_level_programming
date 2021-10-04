a8dde53275a783e8eee0ccbaec7cc30d84eb5be7
Alexander <alex0894rp@gmail.com>
2021-10-04 02:29:33 +0000

    testing restangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,8 +7,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initializes a Rectangle"""
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
