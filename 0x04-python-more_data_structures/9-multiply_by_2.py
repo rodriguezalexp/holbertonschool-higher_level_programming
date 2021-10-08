245785d6c3c06e79303f5f2f7f751fc699c6e18e
Alexander <alex0894rp@gmail.com>
2021-10-08 02:04:15 +0000

    retur a dictionary with all values multiplied by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-
 def multiply_by_2(a_dictionary):
-    cpy = a_dictionary.copy()
-    for key in cpy:
-        cpy[key] *= 2
-    return(cpy)
+    cpy_list = a_dictionary.copy()
+
+    cpy_list.update((x, y*2) for x, y in cpy_list.items())
+    return cpy_list
+
+
+"""for key in a_dictionary.keys():
+        cpy = a_dictionary[key] * val
+    return cpy
+    cpy_list = [a_dictionary[x] * 2 for x in a_dictionary]
+    return dict(cpy_list)
+    return [[round(i / div, 2) for i in l] for l in matrix]"""
