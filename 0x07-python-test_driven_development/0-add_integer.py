563591877d5187ab187396a64aeb076ace99c78c
Alexander <alex0894rp@gmail.com>
2021-10-11 18:57:46 +0000

    Add integer function test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -11,15 +11,14 @@ def add_integer(a, b=98):
     """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
+    if type(b) is not int and type(b) is not float or None:
         raise TypeError("b must be an integer")
-    if type(a) is float:
+    if type(a) is float:        
         a = int(a)
     if type(b) is float:
         b = int(b)
-    return int(a + b)
-
-
+    
+    return a + b
 if __name__ == "__main__":
     import doctest
     doctest.testfile('tests/0-add_integer.txt')
