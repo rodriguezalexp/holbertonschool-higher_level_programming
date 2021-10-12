9bf79950c683b768a72b2bf72f89d69e2b67c0e9
Alexander Rodriguez Pineda <77413136+rodriguezalexp@users.noreply.github.com>
2021-10-11 23:34:48 -0500

    Update 7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,27 +1,18 @@
-#!/usr/bin/python3                                                                                                                            
-"""part of base geometry function                                                                                                             
-                                                                                                                                              
-"""                                                                                                                                           
-                                                                                                                                              
-                                                                                                                                              
-class BaseGeometry:                                                                                                                           
-    def area(self):                                                                                                                           
-        """[summary]                                                                                                                          
-                                                                                                                                              
-        Raises:                                                                                                                               
-            Exception: exeption to check is area is not implement                                                                             
-        """                                                                                                                                   
-        raise Exception("area() is not implemented")                                                                                          
-                                                                                                                                              
-    def integer_validator(self, name, value):                                                                                                 
-        """                                                                                                                                                                                                  
-        """                                                                                                                                   
-                                                                                                                                              
-        if type(value) is not int:                                                                                                            
-            raise TypeError("{:s} must be an integer".format(name))                                                                             
-        if value <= 0 or value < 0:                                                                                                           
+#!/usr/bin/python3
+"""
+Contains the class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """A class with public instance methods area and integer_validator"""
+    def area(self):
+        """raises an exception when called"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates that value is an integer greater than 0"""
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
             raise ValueError("{:s} must be greater than 0".format(name))
-           
-if __name__ == "__main__":                                                                                                                    
-    import doctest                                                                                                                            
-    doctest.testfile('tests/7-base_geometry.txt')
