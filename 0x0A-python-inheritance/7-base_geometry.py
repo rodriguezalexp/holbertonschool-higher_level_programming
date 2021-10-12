b1eb8e0fb4816615e613b3bfab71f2480a72e5dd
Alexander Rodriguez Pineda <77413136+rodriguezalexp@users.noreply.github.com>
2021-10-11 23:31:27 -0500

    Update 7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -14,21 +14,14 @@ class BaseGeometry:
         raise Exception("area() is not implemented")                                                                                          
                                                                                                                                               
     def integer_validator(self, name, value):                                                                                                 
-        """[summary]                                                                                                                          
-                                                                                                                                              
-        Args:                                                                                                                                 
-            name ([str]): name of the object                                                                                                  
-            value ([int]): value of object                                                                                                    
-                                                                                                                                              
-        Raises:                                                                                                                               
-            TypeError: the value must be an integer                                                                                           
-            ValueError: the value must be greather than 0                                                                                     
+        """                                                                                                                                                                                                  
         """                                                                                                                                   
                                                                                                                                               
         if type(value) is not int:                                                                                                            
-            raise TypeError("{} must be an integer".format(name))                                                                             
-        if value == 0 or value < 0:                                                                                                           
-            raise ValueError("{} must be greater than 0".format(name))                                                                                                                                      
+            raise TypeError("{:s} must be an integer".format(name))                                                                             
+        if value <= 0 or value < 0:                                                                                                           
+            raise ValueError("{:s} must be greater than 0".format(name))
+           
 if __name__ == "__main__":                                                                                                                    
     import doctest                                                                                                                            
     doctest.testfile('tests/7-base_geometry.txt')
