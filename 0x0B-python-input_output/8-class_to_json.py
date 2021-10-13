2d11abd6d6b07281903ef5ff51008afccd6dd70b
Alexander Rodriguez Pineda <77413136+rodriguezalexp@users.noreply.github.com>
2021-10-12 22:20:02 -0500

    update changes

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-"""
-Contains the "load_from_json_file" function
-"""
-
-import json
+"""Class to json module"""
 
 
-def load_from_json_file(filename):
-    """creates an Object from a "JSON file" """
-    with open(filename, 'r', encoding='utf-8') as f:
-        return json.load(f)
+def class_to_json(obj):
+	"""Represent a object to json file"""
+	return obj.__dict__
