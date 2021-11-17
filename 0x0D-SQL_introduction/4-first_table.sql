e4a829e8a1a4c80eaacdd0a07ae7ea5fc9139838
Alexander <alexander.0894-rp@hotmail.com>
2021-11-17 04:00:26 +0000

    updating chances

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -4,6 +4,8 @@
 --             id INT
 --             name VARCHAR(256)
 -- The database name will be passed as an argument of the mysql command
+-- If the table first_table already exists, your script should not fail
+-- You are not allowed to use the SELECT or SHOW statements
 
 
-CREATE TABLE first_table IF NOT EXISTS (id INT,name VARCHAR(256)));
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256)));
