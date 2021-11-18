dec0e23128deacfd680598f7b84ebebf98c9cf92
Alexander <alexander.0894-rp@hotmail.com>
2021-11-18 22:28:20 +0000

    list all records of the table second_table

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,1 +1,5 @@
-10-top_score.sql
+-- lists all records of the table second_table.
+-- Results should display both the score and the name (in this order)
+-- Records should be ordered by score (top first)
+
+SELECT score, name FROM second_table ORDER BY score DESC;
