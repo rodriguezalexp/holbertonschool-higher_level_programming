bacddd84932115c4e9b2ed25e9e7b7a16b29de44
Alexander <alexander.0894-rp@hotmail.com>
2021-11-18 22:11:57 +0000

    create second table with all atrbuttes defined

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,1 +1,6 @@
-9-full_creation.sql
+-- Create a second with all attributes defined.
+
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (`id`, `name`, `score`) 
+VALUES (1, "John", 10), (2, "Alex" 3), (3, "Bob", 14), (4, "George" 8);
+
