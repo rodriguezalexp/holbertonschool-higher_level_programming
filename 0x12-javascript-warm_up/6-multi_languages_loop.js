04ce553ec4eba85c63effe0d0442dbc3b303931b
rodriguezalexp <rodriguezalexp@github.com>
2021-12-14 03:19:04 +0000

    print an array

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Include process module;
+const words = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < words.length; i++) {
+  console.log(words[i]);
+}
