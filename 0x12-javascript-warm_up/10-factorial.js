0daf24630b38010b4f189a8b30cdd2da7e84bb7e
rodriguezalexp <rodriguezalexp@github.com>
2021-12-16 00:49:13 +0000

    factorial using recursion in javascript

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,11 @@
-10 - factorial.js
+#!/usr/bin/node
+// program to find the factorial of a number;
+const x = process.argv[2];
+function factorial (x) {
+  if (isNaN(x) || x === 0) {
+    return 1;
+  } else {
+    return x * factorial(x - 1);
+  }
+}
+console.log(factorial(parseInt(x)));
