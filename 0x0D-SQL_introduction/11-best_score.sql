69425f0239843bb6a7bdffc63224211c2cc36464
rodriguezalexp <rodriguezalexp@github.com>
2021-12-31 19:34:25 +0000

    add order

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,4 @@
 -- Lists all records that its score >=10
-SELECT name, score FROM second_table WHERE score >= 10;
+-- Order by score top first
+SELECT name, score FROM second_table WHERE score >= 10
+ORDER BY score DESC;
