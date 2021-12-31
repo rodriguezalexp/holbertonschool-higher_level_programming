78e99f5213d2b06e363cb7cd0cd6c906019619d4
rodriguezalexp <rodriguezalexp@github.com>
2021-12-31 19:37:00 +0000

    new correction

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,4 @@
 -- Lists all records that its score >=10
 -- Order by score top first
 
-SELECT name, score FROM second_table WHERE score >= 10 ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
