f8c10d7e46a0b10e83c094c28add388fb7eb2dbf
vagrant <vagrant@ubuntu2004.localdomain>
2022-04-06 20:10:32 +0000

    new- change

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,14 +1,5 @@
-#!/usr/bin/python3
-def multiply_by_2(a_dictionary):
-    cpy_list = a_dictionary.copy()
-
-    cpy_list.update((x, y*2) for x, y in cpy_list.items())
-    return cpy_list
-
-
-"""for key in a_dictionary.keys():
-        cpy = a_dictionary[key] * val
-    return cpy
-    cpy_list = [a_dictionary[x] * 2 for x in a_dictionary]
-    return dict(cpy_list)
-    return [[round(i / div, 2) for i in l] for l in matrix]"""
+def multiply_by_2(a_dict):
+    new_dict = a_dict.copy()
+    for key in new_dict:
+        new_dict[key] *= 2
+    return new_dict
