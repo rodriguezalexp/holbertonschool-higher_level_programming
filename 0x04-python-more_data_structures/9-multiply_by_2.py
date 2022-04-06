691407adb65db0f57d55169e02051222100f75c3
vagrant <vagrant@ubuntu2004.localdomain>
2022-04-06 20:15:39 +0000

    new- change

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,9 @@
+#!/usr/bin/python3
+"""Funtion to multiply all values of a dict"""
+
+
 def multiply_by_2(a_dictionary):
+    """return values * 2"""
     new_dict = a_dictionary.copy()
     for key in new_dict:
         new_dict[key] *= 2
