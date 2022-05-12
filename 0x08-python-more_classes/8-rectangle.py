dd996e3d45c2600666ec4a67554235e0789bbf2d
Alexander <alexander.0894-rp@hotmail.com>
2022-05-12 04:27:01 +0000

    delete main code into module

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -85,11 +85,3 @@ class Rectangle:
         if rect_1.area() >= rect_2.area():
             return rect_1
         return rect_2
-
-my_rectangle_1 = Rectangle(8, 4)
-my_rectangle_2 = Rectangle(1, 8)
-
-try:
-    print(my_rectangle_2 == Rectangle.bigger_or_equal(my_rectangle_1, "Rect"))
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
